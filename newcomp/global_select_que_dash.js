e8d57ec6e16869804e655550325beabd29a4f73e
{HANGOVERSOUP} <{lkjhkjh8498@naver.com}>
2023-12-15 11:17:35 +0900

    ㅁㅁ

--- newcomp/global_select_que_dash.js
+++ newcomp/global_select_que_dash.js
@@ -1,166 +1,104 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
-import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-
-export default function Top_select({method,method2,page,routed_p,routed_q}) {
-    // console.log("global_select_que_dash_routed_p",routed_p);
-    // console.log("global_select_que_dash_routed_q",routed_q);
-    const router = useRouter();    
-
-    // if (router.query.data===undefined){
-    //     router.query.data = "LG_gram_data"
-    // }
-    // const received = router.query.data;
-    // let cleanedString = received.replace(/\.csv/g, '').replace(/\.xlsx/g, '');
-    // cleanedString = cleanedString.replace(/ /g, '_');
 
 
-    const [surveyOptions, setSurveyOptions] = useState([]);
-    const [selectfile ,setselectfile] = useState(routed_p);
-    const [selectedOptions, setSelectedOptions] = useState(routed_q);
-    const [isSurveyOptionSelected, setIsSurveyOptionSelected] = useState(false);
-    const [isSurveyqueSelected, setisSurveyqueSelected] = useState(false);
+export default function Top_select({ method, method2, page, routed_p, routed_q }) {
+  const router = useRouter();
 
-    const [selectedQuestions, setSelectedQuestions] = useState(routed_q);
-    const [questionOptions, setQuestionOptions] = useState([]);
+  const [surveyOptions, setSurveyOptions] = useState([]);
+  const [selectfile, setSelectfile] = useState(routed_p);
+  const [selectedOptions, setSelectedOptions] = useState(routed_q);
+  const [isSurveyOptionSelected, setIsSurveyOptionSelected] = useState(false);
+  const [isSurveyqueSelected, setIsSurveyqueSelected] = useState(false);
 
+  const [selectedQuestions, setSelectedQuestions] = useState(routed_q);
+  const [questionOptions, setQuestionOptions] = useState([]);
 
-        
-    const handleSelectChange = async (selected) => {
-        const newSelectedOptions = Array.isArray(selected) ? selected : [selected];
-        setSelectedOptions(newSelectedOptions);
-        setIsSurveyOptionSelected(newSelectedOptions.length > 0);
+  const handleSelectChange = async (selected) => {
+    const newSelectedOptions = Array.isArray(selected) ? selected : [selected];
+    setSelectedOptions(newSelectedOptions);
+    setIsSurveyOptionSelected(newSelectedOptions.length > 0);
 
-        if (newSelectedOptions.length > 0) {
-          const selectedOption = newSelectedOptions[0];         
-          const questionOptionUrl = `http://115.68.193.117:9999/net/question-list?p_name=${selectedOption}`;
-          const response = await axios.get(questionOptionUrl);
-          const sur = response.data
-          const newArray = sur.map(item => item.question_text);
-          setQuestionOptions(newArray);
-        }
-    };
+    if (newSelectedOptions.length > 0) {
+      const selectedOption = newSelectedOptions[0];
+      const questionOptionUrl = `http://115.68.193.117:9999/net/question-list?p_name=${selectedOption}`;
+      const response = await axios.get(questionOptionUrl);
+      const sur = response.data;
+      const newArray = sur.map((item) => item.question_text);
+      setQuestionOptions(newArray);
+    }
+  };
 
+  useEffect(() => {
+    method2(surveyOptions, selectedQuestions);
+  }, [selectfile, selectedQuestions]);
 
-    useEffect(() => {
-        // console.log("useEffect_global_select_que_surveyOptions",surveyOptions);
-        // console.log("useEffect_global_select_que_selectedQuestions",selectedQuestions);
-        method2(surveyOptions,selectedQuestions);
-    }, [selectfile,selectedQuestions]);
+  const handleQuestionChange = (selected) => {
+    const newSelectedOptions = Array.isArray(selected) ? selected : [selected];
+    setSelectedQuestions(newSelectedOptions);
+    setIsSurveyqueSelected(newSelectedOptions.length > 0);
+  };
 
-    const handleQuestionChange = (selected) => {
-        const newSelectedOptions = Array.isArray(selected) ? selected : [selected];
-        setSelectedQuestions(newSelectedOptions);
-        setisSurveyqueSelected(newSelectedOptions.length > 0);
-    }; 
-    
-    // 데이터 선택
-    const dataselection = async () => {
-        const surveyOptionsUrl = 'http://115.68.193.117:9999/net/file-list';
-        const response = await axios.get(surveyOptionsUrl);
-        const sur = response.data
-        const newArray = sur.map(item => item.project_name);
-        // console.log("newArray",newArray);
-        setSurveyOptions(newArray);
-    }; 
+  const dataselection = async () => {
+    const surveyOptionsUrl = 'http://115.68.193.117:9999/net/file-list';
+    const response = await axios.get(surveyOptionsUrl);
+    const sur = response.data;
+    const newArray = sur.map((item) => item.project_name);
+    setSurveyOptions(newArray);
+  };
 
-    // API: Fetch survey options
-    useEffect(() => {
-        // console.log("dash_surveyOptions",surveyOptions);
-        // console.log("dash_selectedQuestions",selectedQuestions);
-        // method2(surveyOptions,selectedQuestions);
-        dataselection();
-        // method2(surveyOptions,selectedQuestions);
-    }, []);
+  useEffect(() => {
+    dataselection();
+  }, []);
 
-    const [datalocation, setdatalocation] = React.useState('mo');
-    const handleChange = (event) => {
-        setdatalocation(event.target.value);
-    };
-    const handleChange2 = (event, value) => {
-        handleSelectChange(value);
-        setselectfile(value);
-        // console.log("선택된 파일:", value);
+  const [datalocation, setDatalocation] = React.useState('mo');
+  const handleChange = (event) => {
+    setDatalocation(event.target.value);
+  };
 
-        // method2(value);
-    };
-      
-    const handleChange3 = (event, value) => {
-        // setselectfile(value);
-        // console.log("global_select_que_선택된 질문:", value);
+  const handleChange2 = (event, value) => {
+    handleSelectChange(value);
+    setSelectfile(value);
+  };
 
-        handleQuestionChange(value)
-    };
+  const handleChange3 = (event, value) => {
+    handleQuestionChange(value);
+  };
 
-    const location = ['db','mo'];
-
-    const data = 'mo';
-
-    const top100Films =  surveyOptions;
+  const location = ['db', 'mo'];
+  const data = 'mo';
+  const top100Films = surveyOptions;
 
   return (
-    //1번 프레임 
-    <div>    
-        <div id ='sedi'>
-            <div id='sedi2'>
+    <div>
+      <div id="sedi">
+        <div id="sedi2">
+          <div className="onlyflex">
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={top100Films}
+              sx={{ width: 1340 }}
+              onChange={(event, value) => handleChange2(event, value)}
+              renderInput={(params) => <TextField {...params} label="데이터를 검색하거나 선택하세요." />}
+            />
+          </div>
 
-                <div className='onlyflex'>
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        options={top100Films}
-                        sx={{ width: 1340 }}
-                        onChange={(event, value) => handleChange2(event, value)}
-                        renderInput={(params) => <TextField {...params} label="데이터를 검색하거나 선택하세요." />}
-                        // value={selectfile}
-                    />
-                    {/* <div id='top_radio'>
-                        <FormControl>
-                            <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-                            <RadioGroup
-                            row
-                            aria-labelledby="demo-row-radio-buttons-group-label"
-                            name="row-radio-buttons-group"
-                            >
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                            </RadioGroup>
-                        </FormControl>
-                    </div> */}
-               </div>
-               
-                <div id='dash_options' className='onlyflex'>
-                    {/* {isSurveyOptionSelected && ( */}
-                        <Autocomplete
-                            disablePortal
-                            id="combo-box-demo"
-                            options={questionOptions}
-                            sx={{ width: 1340 }}
-                            onChange={(event, value) => handleChange3(event, value)}
-                            renderInput={(params) => <TextField {...params} label="데이터를 검색하거나 선택하세요." />}
-                            // value={selectedQuestions}
-                        />
-                    {/* )}   */}
-                    
-                    {/* <Button id='chart_make' onClick={method} endIcon={<AutoFixHighIcon />} focusRipple={true} size="large" variant="contained" color="primary">
-                
-                        차트 재생성
-                    </Button> */}
-                </div>
-            </div>
-
+          <div id="dash_options" className="onlyflex">
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={questionOptions}
+              sx={{ width: 1340 }}
+              onChange={(event, value) => handleChange3(event, value)}
+              renderInput={(params) => <TextField {...params} label="데이터를 검색하거나 선택하세요." />}
+            />
+          </div>
         </div>
-        {/* 만드는중 */}
+      </div>
     </div>
-);
+  );
 }
--- pages/new/data-dashboard.js
+++ pages/new/data-dashboard.js
@@ -1,70 +1,52 @@
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import Side_bar from "../../newcomp/global_side_bar";
-import Top_select from "../../newcomp/global_select_que_dash";
-import PieAnimation from "../../newcomp/chart_pie";
-import CombinedChart from "../../newcomp/chart_bar_line";
-import axios from 'axios';
-import { BedRounded } from '@mui/icons-material';
-import AuthCheck from "../../newcomp/login_auth-check";
+import Side_bar from '../../newcomp/global_side_bar';
+import Top_select from '../../newcomp/global_select_que_dash';
+import PieAnimation from '../../newcomp/chart_pie';
+import CombinedChart from '../../newcomp/chart_bar_line';
+import AuthCheck from '../../newcomp/login_auth-check';
+
 export default function Home() {
   const router = useRouter();
 
-  // if (router.query.data===undefined){
-  //     router.query.data = "test1"
-  // }
-  const [receivedData,setreceivedData]=useState(router.query.p_name);
-  const [receivedDataque,setreceivedDataque]=useState(router.query.question);
-  const [good,setgood]=useState("bad");
-  // console.log("dash_router.query",router.query);
-  // console.log("dash_router.query.data",receivedData);
-  // console.log("dash_router.query.question",receivedDataque);
-  // 파일명변동이벤트
-  const filechanged = async (selectfile,selectque) => {
-    if(selectfile!=null){
-      // console.log("dash_변동1",selectfile);
-      // console.log("dash_변동2",selectque);
-      setreceivedData(selectfile);
-      setreceivedDataque(selectque);
-      setgood("good");
+  const [receivedData, setReceivedData] = useState(router.query.p_name);
+  const [receivedDataque, setReceivedDataque] = useState(router.query.question);
+  const [good, setGood] = useState('bad');
+
+  const fileChanged = async (selectfile, selectque) => {
+    if (selectfile != null) {
+      setReceivedData(selectfile);
+      setReceivedDataque(selectque);
+      setGood('good');
     }
-  };                           
+  };
 
-  const i=4;
-  const page=2;
+  const i = 4;
+  const page = 2;
+
   return (
-    //1번 프레임 
-    <div id='outter_frame1'>
-      {/* 왼쪽 바 */}
-      <div id ='left_bar'>
-        <Side_bar index={i}/>
+    <div id="outter_frame1">
+      <div id="left_bar">
+        <Side_bar index={i} />
       </div>
-      {/* 2번 프레임 */}
-      <div id='outter_frame2'>
-          {/* <TopLabel activeStep={activeStepVal}/> */}
-          <Top_select method2={filechanged} page={page} routed_p={receivedData} routed_q={receivedDataque}/>
-          
-
-          {/* 메인: 선택지 , 차트내용 등 */}
-          <div id='main_frame'>
-            <AuthCheck>
-            <div className='onlyflex' id='chart_contain'>
-            {good ==="good" &&(
-            <>
-            
-              <div id='chart_pie' className=''>
-                <PieAnimation file={receivedData} fileque={receivedDataque}/>                  
-              </div>
-
-              <div id='chart_bars'>
-                <CombinedChart file={receivedData} fileque={receivedDataque}/>
-              </div>
-              </>
-            )}
+      <div id="outter_frame2">
+        <Top_select method2={fileChanged} page={page} routed_p={receivedData} routed_q={receivedDataque} />
+        <div id="main_frame">
+          <AuthCheck>
+            <div className="onlyflex" id="chart_contain">
+              {good === 'good' && (
+                <>
+                  <div id="chart_pie">
+                    <PieAnimation file={receivedData} fileque={receivedDataque} />
+                  </div>
+                  <div id="chart_bars">
+                    <CombinedChart file={receivedData} fileque={receivedDataque} />
+                  </div>
+                </>
+              )}
             </div>
-            </AuthCheck>
-            {/* 메인: 선택지 , 차트내용 등 */}              
-          </div>
+          </AuthCheck>
+        </div>
       </div>
     </div>
   );
